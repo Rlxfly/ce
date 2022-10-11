1a7628bea3e8b644c12f0e300a46c699fe35ab44
Rell <me@rlxfly.my.id>
2022-10-11 20:45:02 +0700

    Create var-2.c

---
+++ var-2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+  int age = 17;
+  double gpa = 7.9;
+  char grade = 'B'; // only 1 char
+  char phrase[] = "Re7 Pntx!!!"; // bisa utk bbrp sting/char (bisa dibuat array)
+}
