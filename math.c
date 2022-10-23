9d346d6b5062cb534c43d49821a29b1a3bdf33c1
Rell <me@rlxfly.my.id>
2022-10-23 10:03:50 +0700

    Create math.c

---
+++ math.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  //int num = 69;
+  printf("%f", pow(2, 2));
+  // 2^2 (pangkat) ); //%f = float(45.000000, dst)
+  //%d = desimal (1, 2, 3 ...)
+  printf("%f", sqrt(25)); //sqrt = square root(akar)
+  printf("%f", ceil(36.356)); // bulatkan angka ke atas(tf)
+printf("%f", floor(36.356));
+  // bulatkan angka ke bawah
+}
